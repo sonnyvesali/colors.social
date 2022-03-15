7fb69e36214b411a1bb31203405e99ab6a3cd387
Sonny Vesali <79886185+sovesali@users.noreply.github.com>
2022-03-14 21:16:05 -0500

    home page component styling

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FrontChipsService } from '../services/front-facing-chips/front-chips.service';
-// import { ChipsObj } from '../services/front-facing-chips/front-chips.service';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  constructor(public frontChips: FrontChipsService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.frontChips.getSelectedRoles();
+  opened: boolean = false;
+
+  toggleFilter(ref: MatSidenav) {
+    ref.toggle();
+    this.opened = !this.opened;
   }
+
+  ngOnInit(): void {}
 }
--- src/app/project-cards/services/project-card.service.ts
+++ src/app/project-cards/services/project-card.service.ts
@@ -34,14 +34,13 @@ export class ProjectCardService {
     this.project$ = this.projectRef.valueChanges();
 
     this.project$.subscribe((doc) => {
-      for (let i = 0; i < Object.keys(doc).length - 1; i++) {
-        this.setObject(this.PicObj, `card_${i}`, null);
-        for (let j = 0; j < Object.keys(doc[i].pics_arr).length - 1; j++) {
-          this.setObject(this.PicObj, `card_${j}`, [doc[j].pics_arr]);
-          this.setObject(this.PicObj, `card_${j}.selected`, false);
-        }
+      console.log(Object.keys(doc).length);
+      for (let i = 0; i < Object.keys(doc).length; i++) {
+        this.setObject(this.PicObj, `card_${i}`, [doc[i].pics_arr]);
+        this.setObject(this.PicObj, `card_${i}.selected`, false);
       }
     });
+    console.log(this.PicObj, 'pic objs');
   }
   HoverIn(element: any) {
     this.PicObj[element.id].selected = true;
@@ -54,11 +53,11 @@ export class ProjectCardService {
     this.SelectedID = null;
   }
 
-  changeTheImg(parentElement: HTMLElement, Image: HTMLElement) {
+  changeTheImg(parentElement: any, Image: any) {
     this.imagePath = this.PicObj[parentElement.id][0][Image.id];
   }
 
-  defaultPath(parentElement: HTMLElement) {
+  defaultPath(parentElement: any) {
     this.imagePath = this.PicObj[parentElement.id][0][0];
   }
 }
